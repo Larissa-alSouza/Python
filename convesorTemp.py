0a7765de163998bb182151073d6ec30e2d55f4c2
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-26 12:27:52 -0300

    Create convesorTemp.py

---
+++ convesorTemp.py
@@ -0,0 +1,39 @@
+print("\t\t\t\t\tConversor de Temperatura\n")
+
+def celsiusFahrenheit(num):
+    return (num * 9/5) + 32
+    
+def celsiusKelvin(num):
+    return num + 273.15
+    
+def fahrenheitCelsius(num):
+    return (num - 32) * 5/9
+    
+def fahrenheitKelvin(num):
+    return (num - 32) * 5/9 + 273.15
+
+def kelvinCelsius(num):
+    return num - 273.15
+
+def kelvinFahrenheit(num):
+    return (num - 273.15) * 9/5 + 32
+    
+print("1. Celsius para Fahrenheit\n2. Celsius para Kelvin\n3. Fahrenheit para Celsius\n4. Fahrenheit para Kelvin\n5. Kelvin para Celsius\n6. Kelvin para Fahrenheit\n")
+op = int(input("Escolha uma opção: "))
+
+if op < 1 or op > 6:
+    print("Escolha uma opção válida")
+else:
+    num = int(input("Insira o valor a ser convertido: "))
+    if op == 1:
+        print(f"{num}ºC é igual a {celsiusFahrenheit(num):.2f}ºF")
+    elif op == 2:
+        print(f"{num}ºC é igual a {celsiusKelvin(num):.2f}K")
+    elif op == 3:
+        print(f"{num}ºF é igual a {fahrenheitCelsius(num):.2f}ºC")
+    elif op == 4:
+        print(f"{num}ºF é igual a {fahrenheitKelvin(num):.2f}K")
+    elif op == 5:
+        print(f"{num}K é igual a {kelvinCelsius(num):.2f}ºC")
+    elif op == 6:
+        print(f"{num}K é igual a {kelvinFahrenheit(num):.2f}ºF")
