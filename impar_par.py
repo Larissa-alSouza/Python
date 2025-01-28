d22175cd31f09f85c1188884750f1c2dd83b9db5
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-27 23:14:33 -0300

    Create impar_par.py

---
+++ impar_par.py
@@ -0,0 +1,8 @@
+print("\t\t\t\t\tVerificador de números pares\n")
+
+num = int(input("Insira um número: "))
+
+if num % 2 == 0:
+    print("Este número é par!")
+else:
+    print("Este número é ímpar!")
